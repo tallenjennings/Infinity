d916c596a504f9cf63e844008d52cf010fbbdfb9
Trevor Jennings <tallenjennings@hotmail.com>
2019-05-09 10:08:18 -0600

    added permutation file

---
+++ perm.py
@@ -0,0 +1,20 @@
+
+
+def perm1(lst):
+    if len(lst) == 0:
+        return []
+    elif len(lst) == 1:
+        return[lst]
+    else:
+        l = []
+        for i in range(len(lst)):
+            x = lst[i]
+            xs = lst[:i] + lst[i+1:]
+            for p in perm1(xs):
+                l.append([x]+p)
+        return l
+
+data = list('abc')
+print(perm1)
+for p in perm1(data):
+    print(p)
