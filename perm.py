248e778e42dc63ee6800c0305ef01c97c9012f58
Trevor <tallenjennings@hotmail.com>
2019-05-10 06:07:56 -0600

    second perm

--- perm.py
+++ perm.py
@@ -14,7 +14,33 @@ def perm1(lst):
                 l.append([x]+p)
         return l
 
-data = list([['a'],[['aa'],['ab']]])
+data = list(('abc'))
 print(perm1)
 for p in perm1(data):
+    print(p)
+
+
+
+
+def perm2(lst):
+    if len(lst) == 0:
+        yield []
+    elif len(lst) == 1:
+        yield lst
+    else:
+        for i in range(len(lst)):
+            x = lst[i]
+            xs = lst[:i] + lst[i+1:]
+            for p in perm1(xs):
+                yield [x]+p
+        return l
+
+data = list(('abcdefghij'))
+print(perm1)
+for p in perm1(data):
+    print(p)
+
+data = list(('abcdefghij'))
+print(perm2)
+for p in perm2(data):
     print(p)
