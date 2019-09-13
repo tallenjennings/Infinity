94d6bc863259e22bdf2c94ac2b8a8ac014012610
Trevor <tallenjennings@hotmail.com>
2019-09-13 15:13:45 -0600

    g

--- append.py
+++ append.py
@@ -1,11 +1,13 @@
 char = 'a'
 x = []
+
 for letter in char:
     if letter in char <= chr(1):
         pass
     else:
-        x += chr((ord(char))+1)
+        x = chr((ord(char))+1)
+        y = chr((ord(char))+2)
 
 print(char)
 print(x)
-
+print(y)
