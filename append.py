85eb91f0d677ca7df38563cfc5f98a1aaccefdc3
Trevor Jennings <tallenjennings@hotmail.com>
2019-09-14 07:40:16 -0600

    f

--- append.py
+++ append.py
@@ -1,13 +1,28 @@
 char = 'a'
-x = []
+print(char)
+
 
 for letter in char:
     if letter in char <= chr(1):
         pass
     else:
-        x = chr((ord(char))+1)
-        y = chr((ord(char))+2)
+        x = chr((ord(char))+1) + '[' + char + ']'
+        char = chr((ord(char))+1)
+        y = chr((ord(char))+1) + '[' + char + ']'
+        print(char)
 
-print(char)
+for letter in char:
+    if letter in char <= chr(1):
+        pass
+    else:
+        x = chr((ord(char))+1) + '[' + char + ']'
+        char = chr((ord(char))+1)
+        y = chr((ord(char))+1) + '[' + char + ']'
+        print(char)
+
+
+
+
+#print(char)
 print(x)
 print(y)
