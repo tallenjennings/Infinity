b816612da2950d8b0f9caa83c25477196329b99c
tallenjennings <tallenjennings@hotmail.com>
2019-09-16 12:07:55 -0600

    Add files via upload

--- iter.py
+++ iter.py
@@ -1,8 +1,23 @@
-a=[]
-b = [a]
-ch = 'a'
-x = 25
-for i in range(x):
-    ch = chr((ord(ch)+1))
+#a=[]
+#b = [a]
+#ch = 'a'
+#x = 25
+#for i in range(x):
+#    ch = chr((ord(ch)+1))
+#    
+#    print(ch)
+
+
+
+a1=[]
+b1 = [a1]
+c1 = [b1]
+def inf():
+    print(a1)
+    print(b1)
+    print(c1)
+    c2 = b1+b1
+    print(c2)
     
-    print(ch)
+inf()
+
