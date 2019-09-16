47d2f60d19f31ce1c6ce83650389369c25f62455
tallenjennings <tallenjennings@hotmail.com>
2019-09-16 15:05:48 -0600

    Add files via upload

--- iter.py
+++ iter.py
@@ -9,15 +9,27 @@
 
 
 
-a1=[]
+a1 = ['']
 b1 = [a1]
 c1 = [b1]
+d1 = [c1]
 def inf():
     print(a1)
     print(b1)
     print(c1)
     c2 = b1+b1
     print(c2)
+    print(d1)
+    d2 = [b1+b1]
+    print(d2)
+    d3 = c1+b1
+    print(d3)
+    d4 = b1+c1
+    print(d4)
+    d5 = c2+b1
+    print(d5)
+
+    
     
 inf()
 
