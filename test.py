f8a90e7ad707d8c4194a9ee0e6364ed03855a047
Trevor <tallenjennings@hotmail.com>
2019-09-18 12:31:20 -0600

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+a = []
+ch = 'a'
+b = [a]
+c = [b]
+#def add():
+b = [a]
+c = b + [a]
+d = c + b
+e = d + c
+f = e + d
+#add()
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+#chr((ord(ch)+1))
