138fcd437125a886586683190e39d1b8a1986e74
Trevor <tallenjennings@hotmail.com>
2019-09-18 15:13:36 -0600

    test1

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
-a = []
-ch = 'a'
-b = [a]
-c = [b]
+#a = []
+#ch = 'a'
+#b = [a]
+#c = [b]
 #def add():
-b = [a]
-c = b + [a]
-d = c + b
-e = d + c
-f = e + d
+#b = [a]
+#c = b + [a]
+#d = c + b
+#e = d + c
+#f = e + d
 #add()
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+#print(e)
+#print(f)
 
 #chr((ord(ch)+1))
+
+def iter():
+    if len(lst) == 0:
+            yield []
+    elif len(lst) == 1:
+        yield lst
+
+iter()        
+
+
+list=('a')
+for p in iter():
+    print(list)
+
