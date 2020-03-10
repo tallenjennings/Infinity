32f0c0219d3f54e525feaa46c98281f5da2dae94
tallenjennings <tallenjennings@hotmail.com>
2020-03-09 20:05:24 -0600

    working

--- KeySearch.py
+++ KeySearch.py
@@ -1,61 +1,59 @@
 '''This program will take a key and convert it to binary'''
 
 
-#enter key to be converted
-binaryKey = int(input("enter binary key>>"))
-print("\n")
+binaryKey = int(input("Binary Key>>"))
+ 
+power = 1
+ 
+binary = ""
+ 
+while 2**power < binaryKey:
+    power += 1
+    print("power " + str(power) + ": ")
+    
+sum = 2**power
+ 
+if sum == binaryKey:
+    pass
+elif sum - 1 == binaryKey:
+	pass
+else:
+    power = power - 1
+    print("minus 1 power")
+
+ 
+print("2 ** " + str(power) + " equals: ")
+ 
+print("sum: " + str(sum))
+ 
+base = sum - 1
+print("base: " + str(base))
+ 
+binaryLength = power * "0"
+print("binary length: " + str(binaryLength))
+
+halfPower = (2**int(power))/2
+print("half base: " + str(halfPower))
+
+base = (2**power)-1
+
+	
+
+for char in binaryLength:
+
+	if base + halfPower <= binaryKey:
+	    base = base + halfPower
+	    print("base: " + str(base))
+	    halfPower = halfPower/2
+	    binary = binary + "1"
+	    print("binary 1: " + binary)
+	    
+	else:
+		halfPower = halfPower/2
+		print("base: " + str(base))
+		binary = binary + "0"
+		print("binary 0: " + binary)
+		
 
 
-#key function
-def location(binaryKey):
-
-    count =0
-
-    key =1
-
-    binary = ""
-
-    while key < binaryKey:
-
-        if 2**count < binaryKey:
-
-            print("figuring out the power: ")
-            count += 1
-            print("count: " + str(count))
-            key = (2**(count)/2)-1
-            print("key: " + str(int(key)) + "\n")
-
-        else:
-            break
-
-    while count > 1:
-
-        if (key + ((2**count)/2)) <= binaryKey:
-            key = key + ((2**count)/2)
-            print("Power: ")
-            count -= 1
-            print("count: " + str(count))
-            print("key: " + str(int(key)) + "\n")
-            binary = binary + "1"
-            print("binary: " + binary + "\n")
-
-        #elif (key + ((2**count)/2)) < binaryKey:
-            #key = key + ((2**count)/2)
-            #count -= 1
-            #print("count: " + str(count))
-            #binary = binary + "1"
-
-        else:
-            print("Power: ")
-            count -= 1
-            print("count: " + str(count))
-            print("key: " + str(int(key)) + "\n")
-            binary = binary + "0"
-            print("binary: " + binary + "\n")
-
-
-
-    print("Binary key: " + str(binary))
-
-
-location(binaryKey)
+print("binary solution: " + binary)
